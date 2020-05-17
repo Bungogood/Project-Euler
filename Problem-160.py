8f06e168fdebe2d0145a0f2d9ae5fb73397c3b37
bungogood <github@bungogood.com>
2020-05-17 18:04:50 +0100

    adding solutions

--- Problem-160.py
+++ Problem-160.py
@@ -8,17 +8,16 @@ Factorial trailing digits
 
 def f(x):
     fac = 1
-    p = 0
     for n in range(1, x+1):
-        if n % 10**p == 0:
-            print(n)
-            if n % 10**(p+1) == 0:
-                p+=1
         while n%10==0:
             n //= 10
-        fac *= n
+        fac *= n % 10**5
+        while fac%10==0:
+            fac //= 10
         fac %= 10**5
     return fac
 
 if __name__ == "__main__":
-    print(f(1000000000000))
+    num = f(1000000000000)
+    with open("tmp.txt", "w+") as f:
+        f.write(str(num))
